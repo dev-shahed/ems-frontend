ee9bf43244abf1478d07976dcdda419432f2c0b6
dev-shahed <shahedthedev@gmail.com>
2024-01-23 00:41:37 +0600

    updated production

--- src/services/EmployeeService.jsx
+++ src/services/EmployeeService.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+//https://ems-backend-zlww.onrender.com
 const REST_API_BASE_URL = "https://ems-backend-zlww.onrender.com/api/employees";
 export const getEmployee = () => axios.get(REST_API_BASE_URL);
 // Get employee by id
