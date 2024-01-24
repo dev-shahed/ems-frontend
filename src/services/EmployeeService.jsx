d65a353ce1c101616e9f073770547ee3c1a70510
dev-shahed <shahedthedev@gmail.com>
2024-01-24 21:50:09 +0600

    fix api base url dynamically

--- src/services/EmployeeService.jsx
+++ src/services/EmployeeService.jsx
@@ -1,13 +1,11 @@
 import axios from "axios";
 
-const rootElement = document.getElementById("root");
 const REST_API_BASE_URL = import.meta.env.DEV
   ? import.meta.env.VITE_API_URL_LOCAL
   : import.meta.env.VITE_API_URL_PROD;
 
-//https://ems-backend-zlww.onrender.com
-// const REST_API_BASE_URL = "https://ems-backend-zlww.onrender.com/api/employees";
-// const REST_API_BASE_URL = "http://localhost:8080/api/employees";
+
+console.log(REST_API_BASE_URL);
 
 //get all employee..
 export const getEmployee = () => axios.get(REST_API_BASE_URL);
