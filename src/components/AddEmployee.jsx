1fe8c65fce1be380054529cfa83ab19e0ca63539
dev-shahed <shahedthedev@gmail.com>
2024-01-25 01:56:39 +0600

    curd done employee with few ui fixes

--- src/components/AddEmployee.jsx
+++ src/components/AddEmployee.jsx
@@ -80,15 +80,15 @@ export default function AddEmployee() {
   };
 
   return (
-    <div className="flex flex-col items-center min-h-screen pt-6 bg-gray-100 sm:justify-center sm:pt-0">
-      <div className="px-6 py-8 mt-6 overflow-hidden bg-white rounded-lg lg:max-w-4xl">
+    <div className="flex flex-col items-center min-h-screen pt-8 bg-gray-100">
+      <div className="max-w-4xl px-4 py-8 mt-6 overflow-hidden bg-white rounded-lg sm:px-6 md:px-8">
         <div className="mb-4">
-          <h1 className="font-serif text-2xl font-bold text-gray-800">
+          <h1 className="font-serif text-xl font-bold text-gray-800 sm:text-2xl md:text-3xl">
             {isUpdate ? "Updating" : "Adding"} an Employee
           </h1>
         </div>
 
-        <div className="px-6 py-8 bg-white rounded shadow-md ring-1 ring-gray-900/10">
+        <div className="px-4 py-8 bg-white rounded shadow-md sm:px-6 md:px-8 ring-1 ring-gray-900/10">
           <form onSubmit={saveEmployee}>
             <Input
               label="First Name"
@@ -120,7 +120,7 @@ export default function AddEmployee() {
             <div className="flex items-center justify-between mt-6">
               <button
                 type="submit"
-                className="px-6 py-2 text-sm font-semibold text-white bg-indigo-500 rounded-md shadow-md hover:bg-indigo-700 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300"
+                className="px-4 py-2 text-sm font-semibold text-white bg-indigo-500 rounded-md shadow-md sm:px-6 md:text-base hover:bg-indigo-700 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300"
               >
                 {isUpdate ? "Update" : "Save"}
               </button>
@@ -128,7 +128,7 @@ export default function AddEmployee() {
               <button
                 type="button"
                 onClick={() => navigator("/")}
-                className="px-6 py-2 text-sm font-semibold text-gray-800 bg-gray-200 rounded-md shadow-md hover:bg-gray-300 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300"
+                className="px-4 py-2 text-sm font-semibold text-gray-800 bg-gray-200 rounded-md shadow-md sm:px-6 md:text-base hover:bg-gray-300 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300"
               >
                 Cancel
               </button>
