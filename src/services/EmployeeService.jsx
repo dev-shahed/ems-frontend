02a7b7ddbf6241c021514285eb1ed48ddd894d4e
dev-shahed <shahedthedev@gmail.com>
2024-01-24 21:29:30 +0600

    add env and update gitignore to exclude credentials

--- src/services/EmployeeService.jsx
+++ src/services/EmployeeService.jsx
@@ -1,8 +1,13 @@
 import axios from "axios";
 
+const rootElement = document.getElementById("root");
+const REST_API_BASE_URL = import.meta.env.DEV
+  ? import.meta.env.VITE_API_URL_LOCAL
+  : import.meta.env.VITE_API_URL_PROD;
+
 //https://ems-backend-zlww.onrender.com
-//const REST_API_BASE_URL = "https://ems-backend-zlww.onrender.com/api/employees";
-const REST_API_BASE_URL = "http://localhost:8080/api/employees";
+// const REST_API_BASE_URL = "https://ems-backend-zlww.onrender.com/api/employees";
+// const REST_API_BASE_URL = "http://localhost:8080/api/employees";
 
 //get all employee..
 export const getEmployee = () => axios.get(REST_API_BASE_URL);
