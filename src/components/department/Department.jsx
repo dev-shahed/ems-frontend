1eecc119b368ccf12c3d319334c2a3b2c06bb3d1
dev-shahed <shahedthedev@gmail.com>
2024-01-27 02:30:32 +0600

    re-design adding dept form again

--- src/components/department/Department.jsx
+++ src/components/department/Department.jsx
@@ -1,65 +1,68 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment } from "react";
+
+const Input = ({ label, name, type, placeholder, onChange, value }) => (
+  <div className="mt-4">
+    <label className="block text-sm font-bold text-gray-700" htmlFor={name}>
+      {label}
+    </label>
+    <input
+      className="block w-full px-4 py-2 mt-1 border rounded-md shadow-sm focus:outline-none focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+      type={type}
+      name={name}
+      placeholder={placeholder}
+      onChange={onChange}
+      value={value}
+    />
+  </div>
+);
 
 export default function Department() {
-  const [department, setDepartment] =
-    useState[
-      {
-        name: "",
-        description: "",
-      }
-    ];
-
-
-
-
-  const inputStyles =
-    "block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-100 focus:ring-blue-500 focus:border-blue-500";
-  const labelStyles = "block mb-2 text-sm font-medium text-black";
-
-  const buttonStyles =
-    "inline-block px-4 py-2 mr-2 text-sm font-medium text-white rounded-md";
-  const submitButtonStyles =
-    "bg-green-500 hover:bg-green-600 focus:outline-none focus:ring focus:border-green-300";
-  const cancelButtonStyles =
-    "bg-red-500 hover:bg-red-600 focus:outline-none focus:ring focus:border-red-300";
   return (
     <Fragment>
-      <form className="max-w-sm mx-auto bg-gray-200 p-6 rounded-md shadow-md my-10">
-        <div className="text-xl font-bold mb-4 text-center text-indigo-800">
-          Add Department
+      <form className="max-w-sm mx-auto bg-white p-6 mt-10 rounded-md shadow-md">
+        <div className="mb-4">
+          <h1 className="font-serif text-xl font-bold text-gray-800 sm:text-2xl md:text-3xl">
+            Adding a Department
+          </h1>
         </div>
         <div className="mb-5">
-          <label htmlFor="departmentName" className={labelStyles}>
+          <label
+            htmlFor="departmentName"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
             Department Name
           </label>
           <input
             type="text"
             id="departmentName"
             name="departmentName"
-            className={`${inputStyles} sm:text-md dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+            className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-100 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-100 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
           />
         </div>
         <div className="mb-5">
-          <label htmlFor="message" className={labelStyles}>
+          <label
+            htmlFor="description"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
             Description
           </label>
           <textarea
-            id="message"
+            id="description"
             rows="4"
-            className={`${inputStyles} block p-2.5 text-sm dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
-            placeholder="Department Descriptions..."
+            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-100 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-100 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Leave a comment..."
           ></textarea>
         </div>
         <div className="flex justify-between">
           <button
             type="submit"
-            className={`${buttonStyles} ${submitButtonStyles}`}
+            className="px-4 py-2 text-sm font-semibold text-white bg-indigo-500 rounded-md shadow-md sm:px-6 md:text-base hover:bg-indigo-700 focus:outline-none focus:border-indigo-900 focus:ring ring-indigo-300"
           >
-            Submit
+            Save
           </button>
           <button
             type="button"
-            className={`${buttonStyles} ${cancelButtonStyles}`}
+            className="px-4 py-2 text-sm font-semibold text-gray-800 bg-gray-200 rounded-md shadow-md sm:px-6 md:text-base hover:bg-gray-300 focus:outline-none focus:border-indigo-900 focus:ring ring-indigo-300"
           >
             Cancel
           </button>
